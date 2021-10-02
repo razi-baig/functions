f2a8aff02c16ce98deb6c752cf6775193b882b11
razi ullah baig <raziullahbaig70@gmail.com>
2021-10-02 14:38:28 +0500

    methods

---
+++ dom.js
@@ -0,0 +1,166 @@
+// let x = [
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHwxfHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " Cp6s3jERPl",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHwyfHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " X9geZuQ4",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1495360010541-f48722b34f7d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHwzfHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " Fb",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1561948955-570b270e7c36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHw0fHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " 6Vtedj5yslU51peKwy",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1548247416-ec66f4900b2e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHw1fHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " EwHEx4fsVt10QtzMn82",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1543852786-1cf6624b9987?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHw2fHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " wDY6Qmngh072vOWahJwlxq0o",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1511044568932-338cba0ad803?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHw3fHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " KaJqZZa",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1574158622682-e40e69881006?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHw4fHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " 1LPVKoPE",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1501820488136-72669149e0d4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHw5fHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " 8l7dg6X2W5U0RwF",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1478098711619-5ab0b478d6e6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHwxMHx8Y2F0fGVufDB8fHx8MTYyMTk0MDU5Mw&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " FBwg",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHwxfHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " KTetn1wcGHldyRtNtUi9Vd0",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHwyfHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " wO0IDE99WI",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1501820488136-72669149e0d4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHw5fHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " EB6qVyYTV1P",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1478098711619-5ab0b478d6e6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHwxMHx8Y2F0fGVufDB8fHx8MTYyMTk0MDU5Mw&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " yyAvhaSG",
+//   },
+//   {
+//     image:
+//       "https://images.unsplash.com/photo-1511044568932-338cba0ad803?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzAzODZ8MHwxfHNlYXJjaHw3fHxjYXR8ZW58MHx8fHwxNjIxOTQwNTkz&ixlib=rb-1.2.1&q=80&w=1080",
+//     imageName: " HItFouW78",
+//   },
+// ];
+// let a = document.querySelector(".card");
+
+// for (let i = 0; i < x.length; i++) {
+//   a.innerHTML += ` <img src=" ${x[i].image}"</>
+//   <h1>${x[i].imageName}</h1> `;
+// }
+// const formElem = document.getElementById("form");
+// formElem.addEventListener("submit", function (event) {
+//   console.log(event);
+//   event.preventDefault();
+//   let fd = new FormData(event.target);
+//   const values = Object.fromEntries(fd.entries());
+//     console.log(values);
+// });
+// const usernameEle = document.getElementById("username");
+// usernameEle.addEventListener("change", function (e) {
+//   console.log(e.target.value);
+//   let v1 = document.getElementById("v");
+//   if (e.target.value == "") {
+//     v1.innerHTML = '<h1 style="color: red">username cannot be empyt.</h1>';
+//   } else {
+//     v1.innerHTML = "";
+//   }
+// });
+
+// forEach Filter Map
+// function forEach(array = [], cb) {
+//   for (let i = 0; i < array.length; i++) {
+//     cb(array[i], i, array);
+//   }
+// // }
+// let array = [1, 2, 35, 6, 7, 8, 9, 9, 4, 343];
+// forEach(array, function (currentItem, index, wArr) {
+//   console.log("custom forEach", index, currentItem, wArr);
+// });
+// function map(array = [], cb) {
+//   let newArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     newArray.push(cb(array[i], i, array));
+//   }
+//   return newArray;
+// }
+// let mapOutput = map(array, function (currItem) {
+//   return currItem * 2;
+// });
+// // console.log("mapOutput", mapOutput);
+// function filter(array = [], cb) {
+//   let newArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (cb(array[i], i, array)) {
+//       newArray.push(array[i]);
+//     }
+//   }
+//   return newArray;
+// }
+// let filterOutput = filter(array, function (currItem) {
+//   return currItem > 100;
+// });
+// console.log(filterOutput);
+// array.forEach(function (currentItem, index, wArr) {
+//   console.log("array method forEach", index, currentItem, wArr);
+// });
+// let output2 = array.map(function (vlaue) {
+//   return "lay";
+// });
+// console.log(output2);
+// let output3 = array.filter(function (vlaue) {
+//   return vlaue > 100;
+// });
+// console.log(output3);
+
+// Practice
+var hunza = ["altit", "aliabad", "karimabad", "hyderabad", "ganish"];
+// console.log(typeof hunza);
+// hunza.forEach(function (currentItem, index, whole_array) {
+//   console.log("for forEach Method", currentItem, index, whole_array);
+// });
+
+// let output2 = hunza.map(function (vlaue, hello, hehe) {
+//   return vlaue;
+// });
+// console.log(output2);
+// map(hunza, function (currItem) {
+//   return currItem;
+// });
+let output3 = hunza.filter(function (value, hello, hehe) {
+  return hehe == "altit";
+});
+console.log(output3);
